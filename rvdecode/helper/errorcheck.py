28c12fbf3838ebcae5f94aabbcad03b375f39169
Zach Mascetta <zmascetta@protonmail.com>
2025-10-08 17:54:46 -0400

    Added argument_check and instruction_check functions in errorcheck mod. Also added system_exit function to provide uniform exiting.

---
+++ rvdecode/helper/errorcheck.py
@@ -0,0 +1,43 @@
+import sys, re
+
+
+# Check to make sure only 1 arg has been entered.
+def argument_check(arguments):
+    if len(arguments) != 2:
+        system_exit()
+
+
+# Check to ensure that either a 32-bit bin instr or 8-bit hex instr has been entered.
+# Will return binary instruction.
+def instruction_check(instruction):
+    # Clear any spaces if passed in with quotes
+    instruction = instruction.replace(' ', '')
+    # Check for 32-bit binary instruction.
+    binary_check = re.search(
+        r"^[0,1]{32}$",
+        instruction)
+
+    # Check for 8-bit hex instruction.
+    hex_check = re.search(
+        r"^[0-9a-fA-F]{8}$",
+        instruction)
+
+    # Return binary instruction or end program if both checks have failed.
+    if binary_check is not None:
+        return instruction
+    elif hex_check is not None:
+        instruction = int(instruction, base=16)
+        instruction = str(bin(instruction))[2:].zfill(32)
+        return instruction
+    else:
+        system_exit()
+
+# Function for exiting system.
+# Accepts additional lines if necessary.
+def system_exit(extra_lines=None):
+    error_message = "Usage:\n" \
+                    "\t\033[1mrvdecode\033[0m instruction\n" \
+                    "\tInstruction must be a valid 32-bit binary or 8-bit hex instruction."
+    if extra_lines is not None:
+        error_message += "\n" + extra_lines
+    sys.exit(error_message)
