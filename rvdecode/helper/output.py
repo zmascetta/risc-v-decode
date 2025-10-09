98fa6a5a60b02561002611710686055a2c3697ca
Zach Mascetta <zmascetta@protonmail.com>
2025-10-09 17:55:02 -0400

    WIP.

---
+++ rvdecode/helper/output.py
@@ -0,0 +1,19 @@
+
+# specific type - use func, store specific, add, addi, etc.
+# immediate decode - store, convert to hex and dec
+# registers decode - store, convert to ABI
+# make assembly line
+#
+# output
+
+def lookup_instruction(opcode, func3=None, func7=None):
+    pass
+
+
+def immediate_conversion(immediate):
+    pass
+
+
+def register_conversion(register_value):
+    register {name, alias, use}
+    pass
