0487d1487649d15fb4dfac2915dcb57ddcf192f2
Zach Mascetta <zmascetta@protonmail.com>
2025-10-09 18:43:59 -0400

    WIP.

--- rvdecode/helper/output.py
+++ rvdecode/helper/output.py
@@ -1,19 +1,37 @@
+'''
+specific type - use func, store specific, add, addi, etc.
 
-# specific type - use func, store specific, add, addi, etc.
-# immediate decode - store, convert to hex and dec
-# registers decode - store, convert to ABI
-# make assembly line
-#
-# output
+immediate decode - store, convert to hex and dec
+registers decode - store, convert to ABI
+make assembly line
+
+0000000 00001 00000 000 00101 0010011
+00000000000100000000001010010011
+'''
 
 def lookup_instruction(opcode, func3=None, func7=None):
     pass
 
 
 def immediate_conversion(immediate):
-    pass
+    immediate_data = {"value": immediate,
+                        "bin": "0b" + str(int(immediate, base=2)),
+                        "dec": str(int(immediate)),
+                        "hex": "0x"+ str(int(immediate, base=16))}
+    return immediate_data
 
 
 def register_conversion(register_value):
-    register {name, alias, use}
+    register_data = {"value": register_value,
+                     "name": "asdf",
+                     "alias": "asdf",
+                     "use": "asdf"
+                     }
     pass
+
+
+def print_output(decoded_instruction):
+    pass
+
+imm = immediate_conversion("000000000001")
+print(imm)
