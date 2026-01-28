18447180dbb50902af076b6eceecdce985a8cc4e
Zach Mascetta <zmascetta@protonmail.com>
2026-01-28 13:31:34 -0500

    Finished s-type. Updated output to handle "basic offset" printing (i.e., not explanation of bitshifting/extension).

--- rvdecode/modules/instructions/s-type/s-type.py
+++ rvdecode/modules/instructions/s-type/s-type.py
@@ -1,44 +1,44 @@
-from rvdecode.modules.helper import decode as decode, errorcheck as errorcheck
-
+from rvdecode.modules.helper import decode as decode, errorcheck as errorcheck, header as header, convert as convert
 
 '''
 S-Instruction:
-Components: Opcode, Func3, RS1, RS2
+Components: Opcode, Func3, RS1, RS2, Offset
 '''
-def s_instruction(instruction, opcode, func3):
-    # Create header dict.
-    header_info = create_header_info(instruction, "s_instruction")
+def decode_instruction(instruction, opcode):
+    func3 = decode.get_func3(instruction)
+    lookup_value = opcode+func3
+    spacing_list = (7, 5, 5, 3, 5, 7)
+    spacing_label = "off---| rs2-| rs1-| f3| off-| opcode|\n"
 
-    # func3 is lookup value
-    instruction_lookup = {
-        "000": {"short_name": "sb", "full_name": "store byte"},
-        "001": {"short_name": "sh", "full_name": "store halfword"},
-        "010": {"short_name": "sw", "full_name": "store word"}}
-    header_info.update(decode.instruction_lookup(func3, instruction_lookup, "Invalid func3."))
+    # Create header dict.
+    header_info = header.create_header_info(instruction, spacing_list, spacing_label, "s-type", lookup_value)
 
     # Create general info dict.
     general_info = {"instruction_type": "S-Type",
-                    "instruction_set": INSTRUCTION_SET,
+                    "source": header_info["source"],
                     "opcode": opcode,
                     "func3": func3}
 
     # Create rs1 dict.
-    rs1_info = decode.register_conversion(decode.get_rs1(instruction))
+    rs1_info = convert.register_conversion(decode.get_rs1(instruction))
 
     # Create rs2 dict.
-    rs2_info = decode.register_conversion(decode.get_rs2(instruction))
+    rs2_info = convert.register_conversion(decode.get_rs2(instruction))
 
-    # Create assembly info dict
-    assembly_info = {"name": header_info["short_name"],
-                     "rs1": rs1_info["alias"],
-                     "rs2": rs2_info["alias"],
-                     }
+    # Create offset dict
+    offset = instruction[0:8] + instruction[20:25]
+    off_info = {"binary_value": offset}
+    off_info.update(convert.signed_conversion(offset))
+
+    # Create assembly text
+    assembly_text = decode.make_assembly_code(header_info["short_name"], rs1=rs1_info["alias"], rs2=rs2_info["alias"], imm=off_info["decimal_value"])
 
     # Add all dicts to decoded instruction dict
     decoded_instruction = {"header_info": header_info,
                            "general_info": general_info,
                            "rs1_info": rs1_info,
                            "rs2_info": rs2_info,
-                           "assembly_info": assembly_info}
+                           "off_info": off_info,
+                           "assembly_info": assembly_text}
 
     return decoded_instruction
